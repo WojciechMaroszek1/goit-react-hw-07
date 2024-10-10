fbb4f477d68108940747183cc54b2c3241d84f20
Wojciech Maroszek <122974346+WojciechMaroszek1@users.noreply.github.com>
2024-10-10 20:08:35 +0200

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,29 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // default localstorage for web
 import contactsReducer from './contactsSlice.js';
 import filtersReducer from './filtersSlice.js';
 
-const persistConfig = {
-	key: 'root',
-	storage,
-};
-
-const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
-
 const store = configureStore({
 	reducer: {
-		contacts: persistedContactsReducer,
+		contacts: contactsReducer,
 		filters: filtersReducer,
 	},
 	middleware: getDefaultMiddleware =>
 		getDefaultMiddleware({
-			serializableCheck: {
-				ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
-			},
+			serializableCheck: false,
 		}),
 });
 
-const persistor = persistStore(store);
-
-export { store, persistor };
+export { store };
